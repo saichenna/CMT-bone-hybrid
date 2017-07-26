571e29eabfa0a9ac569b45ceaed288fd9ccb3e0c
saichenna <chenna@hcs.ufl.edu>
2017-07-26 10:42:38 -0400

    Minor error fixes

--- HybrideCMTBone-newcurtainTestcase-master/nek5/examples/new3dbox-32768-128-lb/particles-cuda.cu
+++ HybrideCMTBone-newcurtainTestcase-master/nek5/examples/new3dbox-32768-128-lb/particles-cuda.cu
@@ -71,7 +71,7 @@ __global__ void particles_in_nid(int *fptsmap, double *rfpts, int *ifpts, double
 }
 
 
-__global__ void update_particle_location(double *rpart1, int *ipart, double *xdrange1, int *in_part, int *bc_part, int ndim, int nr, int ni, int n, int jx, int jx1, int jx2, int jx3){
+__global__ void update_particle_location(double *rpart1, double *xdrange1, int *in_part, int *bc_part, int n, int ndim, int nr, int jx, int jx1, int jx2, int jx3){
     int id = blockIdx.x*blockDim.x+threadIdx.x;
     if(id < n*ndim){
           int i = id/ndim;
@@ -107,126 +107,130 @@ __global__ void update_particle_location(double *rpart1, int *ipart, double *xdr
 }
 
 
-__global__ void interp_props_part_location(double *rpart,double *vx,double *vy,double *vz,double *t,double *vtrans,double *rep,double *xgll,double *ygll,double *zgll,double *wxgll,double *wygll,double *wzgll,int nx1r,int nx1,int nr,int jr,int ju0, int je0,int jtemp, int jrho){
+__global__ void interp_props_part_location(double *rpart, int *ipart, double *vx,double *vy,double *vz,double *t,double *vtrans,double *rep,double *xgll,double *ygll,double *zgll,double *wxgll,double *wygll,double *wzgll,int n, int nr, int ni, int nx1, int nx1r, int jr, int ju0, int je0, int jtemp, int jrho){
     int id = blockIdx.x*blockDim.x+threadIdx.x;
-    double x,y,z;
-    double bot = 0.0;
-    double diff;
-    double repdum;
-    double bwgtz[nx1];
-    double bwgty[nx1];
-    double bwgtx[nx1];
-    x = rpart[id*nr+jr];
-    y = rpart[id*nr+jr+1];
-    z = rpart[id*nr+jr+2];
-// barycentric interpolation initialization
-    for (int k = 0; k < nx1r; k++){
-          diff = z - zgll[k];
-          if (fabs(diff) < 1.0e-16){
-                if (diff > 0){
-                      diff = 1.0e-16;
-                }
-                else{
-                      diff = -1.0e-16;
-                }
+    if (id < n){
+      double x,y,z;
+      double bot = 0.0;
+      double diff;
+      double repdum;
+      double bwgtz[nx1];
+      double bwgty[nx1];
+      double bwgtx[nx1];
+      x = rpart[id*nr+jr];
+      y = rpart[id*nr+jr+1];
+      z = rpart[id*nr+jr+2];
+  // barycentric interpolation initialization
+      for (int k = 0; k < nx1r; k++){
+            diff = z - zgll[k];
+            if (fabs(diff) < 1.0e-16){
+                  if (diff > 0){
+                        diff = 1.0e-16;
+                  }
+                  else{
+                        diff = -1.0e-16;
+                  }
 
-          }
-          bwgtz[k] = wzgll[k]/diff;
-   }
-   for (int i = 0; i < nx1r; i++){
-         diff = x - xgll[i];
-         if (fabs(diff) < 1.0e-16){
+            }
+            bwgtz[k] = wzgll[k]/diff;
+     }
+     for (int i = 0; i < nx1r; i++){
+           diff = x - xgll[i];
+           if (fabs(diff) < 1.0e-16){
+                 if (diff > 0){
+                       diff = 1.0e-16;
+                 }
+                 else{
+                       diff = -1.0e-16;
+                 }
+
+           }
+           bwgtx[i] = wxgll[i]/diff;
+    }
+    for (int j = 0; j < nx1r; j++){
+          diff = y - ygll[j];
+          if (fabs(diff) < 1.0e-16){
                if (diff > 0){
-                     diff = 1.0e-16;
+                      diff = 1.0e-16;
                }
                else{
-                     diff = -1.0e-16;
+                      diff = -1.0e-16;
                }
 
+          }
+          bwgty[j] = wygll[j]/diff;
+   }
+
+   for(int k=0; k<nx1r;k++){
+         for(int j=0; j<nx1r; j++){
+             repdum = bwgty[j] * bwgtz[k] ;
+             for(int i = 0; i<nx1r; i++){
+                 rep[k*nx1r*nx1r+j*nx1r+i] = repdum * bwgtx[i];
+                 bot = bot + rep[k*nx1*nx1+j*nx1+i];
+             }
          }
-         bwgtx[i] = wxgll[i]/diff;
-  }
-  for (int j = 0; j < nx1r; j++){
-        diff = y - ygll[j];
-        if (fabs(diff) < 1.0e-16){
-             if (diff > 0){
-                    diff = 1.0e-16;
-             }
-             else{
-                    diff = -1.0e-16;
-             }
+     }
+  for(int k=0; k<nx1r;k++){
+        for(int j=0; j<nx1r; j++){
+              for(int i = 0; i<nx1r; i++){
+                    rep[k*nx1r*nx1r+j*nx1r+i] = rep[k*nx1r*nx1r+j*nx1r+i]/bot;
+              }
 
         }
-        bwgty[j] = wygll[j]/diff;
- }
 
- for(int k=0; k<nx1r;k++){
-       for(int j=0; j<nx1r; j++){
-           repdum = bwgty[j] * bwgtz[k] ;
-           for(int i = 0; i<nx1r; i++){
-               rep[k*nx1r*nx1r+j*nx1r+i] = repdum * bwgtx[i];
-               bot = bot + rep[k*nx1*nx1+j*nx1+i];
-           }
-       }
-   }
-for(int k=0; k<nx1r;k++){
-      for(int j=0; j<nx1r; j++){
-            for(int i = 0; i<nx1r; i++){
-                  rep[k*nx1r*nx1r+j*nx1r+i] = rep[k*nx1r*nx1r+j*nx1r+i]/bot;
-            }
+  }
 
-      }
+  // barycentric interpolation initialization ends  //
 
-}
+  int ie = ipart[id*ni+je0];
+  double top1 = 0.0, top2 = 0.0, top3 = 0.0, top4 = 0.0, top5 = 0.0;
+  int nxyz = nx1*nx1*nx1;
+  double *fieldx = vx+ie*nxyz;
+  double *fieldy = vy+ie*nxyz;
+  double *fieldz = vz+ie*nxyz;
+  double *fieldt = t+ie*nxyz;
+  double *fieldvtrans = vtrans+ie*nxyz;
+  //full interpolation
+  if (nx1r == nx1){
+        for (int i=0; i< nxyz; i++){
+              top1 = top1 + rep[i]*fieldx[i];
+              top2 = top2 + rep[i]*fieldy[i];
+              top3 = top3 + rep[i]*fieldz[i];
+              top4 = top4 + rep[i]*fieldt[i];
+              top5 = top5 + rep[i]*fieldvtrans[i];
+        }
+        rpart[id*nr+ju0] = top1;
+        rpart[id*nr+ju0+1] = top2;
+        rpart[id*nr+ju0+2] = top3;
+        rpart[id*nr+jtemp] = top4;
+        rpart[id*nr+jrho] = top5;
+  }
 
-// barycentric interpolation initialization ends  //
+  else{
+        // reduced barycentric interpolation
+        int kk = 0,jj = 0, ii = 0;
+        double ijk3,ijk2,ijk1;
+        for (int k=0;k < nx1; k+=2){
+              for (int j=0;j<nx1;j+=2){
+                    for (int i=0;i<nx1;i+=2){
+                          top1 = top1+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
+                          top2 = top2+rep[k*nx1*nx1+j*nx1+i]*fieldy[k*nx1*nx1+j*nx1+i];
+                          top3 = top3+rep[k*nx1*nx1+j*nx1+i]*fieldz[k*nx1*nx1+j*nx1+i];
+                          top4 = top4+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
+                          top5 = top5+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
+                    }
+              }
+        }
+        rpart[id*nr+ju0] = top1;
+        rpart[id*nr+ju0+1] = top2;
+        rpart[id*nr+ju0+2] = top3;
+        rpart[id*nr+jtemp] = top4;
+        rpart[id*nr+jrho] = top5;
 
-int ie = ipart[id*ni+je0];
-double top1 = 0.0, top2 = 0.0, top3 = 0.0, top4 = 0.0, top5 = 0.0;
-int nxyz = nx1*nx1*nx1;
-double *fieldx = vx+ie*nxyz;
-double *fieldy = vy+ie*nxyz;
-double *fieldz = vz+ie*nxyz;
-double *fieldt = t+ie*nxyz;
-double *fieldvtrans = vtrans+ie*nxyz;
-//full interpolation
-if (nx1r == nx1){
-      for (int i=0; i< nxyz; i++){
-            top1 = top1 + rep[i]*fieldx[i];
-            top2 = top2 + rep[i]*fieldy[i];
-            top3 = top3 + rep[i]*fieldz[i];
-            top4 = top4 + rep[i]*fieldt[i];
-            top5 = top5 + rep[i]*fieldvtrans[i];
-      }
-      rpart[id*nr+ju0] = top1;
-      rpart[id*nr+ju0+1] = top2;
-      rpart[id*nr+ju0+2] = top3;
-      rpart[id*nr+jtemp] = top4;
-      rpart[id*nr+jrho] = top5;
-}
+  }
 
-else{
-      // reduced barycentric interpolation
-      int kk = 0,jj = 0, ii = 0;
-      double ijk3,ijk2,ijk1;
-      for (int k=0;k < nx1; k+=2){
-            for (int j=0;j<nx1;j+=2){
-                  for (int i=0;i<nx1;i+=2){
-                        top1 = top1+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
-                        top2 = top2+rep[k*nx1*nx1+j*nx1+i]*fieldy[k*nx1*nx1+j*nx1+i];
-                        top3 = top3+rep[k*nx1*nx1+j*nx1+i]*fieldz[k*nx1*nx1+j*nx1+i];
-                        top4 = top4+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
-                        top5 = top5+rep[k*nx1*nx1+j*nx1+i]*fieldx[k*nx1*nx1+j*nx1+i];
-                  }
-            }
-      }
-      rpart[id*nr+ju0] = top1;
-      rpart[id*nr+ju0+1] = top2;
-      rpart[id*nr+ju0+2] = top3;
-      rpart[id*nr+jtemp] = top4;
-      rpart[id*nr+jrho] = top5;
+  }
 
-}
 }
 
 //-----------------------------------------------------------
@@ -463,7 +467,7 @@ extern "C" void particles_in_nid_wrapper_(int *fptsmap, double *rfpts, int *ifpt
 
         }
         //free
-	cudaFree(d_rpart);
+	      cudaFree(d_rpart);
         cudaFree(d_ipart);
         cudaFree(d_xerange);
         cudaFree(d_fptsmap);
@@ -489,7 +493,7 @@ extern "C" void particles_in_nid_wrapper_(int *fptsmap, double *rfpts, int *ifpt
 
 }
 
-extern "C" void update_particle_location_wrapper_(double *rpart, int *ipart, double *xdrange, int *in_part, int *bc_part, int *ndim, int *nr, int *ni, int *n, int *jx, int *jx1, int *jx2,int *jx3){
+extern "C" void update_particle_location_wrapper_(double *rpart, double *xdrange, int *in_part, int *bc_part, int *n, int *ndim, int *nr, int *jx, int *jx1, int *jx2,int *jx3){
 
     float time;
     cudaEvent_t startEvent, stopEvent;
@@ -499,18 +503,18 @@ extern "C" void update_particle_location_wrapper_(double *rpart, int *ipart, dou
 
     bool inCPU = false;
     double *d_rpart, *d_xdrange;
-    int *d_ipart, *d_in_part;
-    int ic = 0;
+    int *d_in_part;*d_bc_part;
+//    int ic = 0;
     if(inCPU){
 
         cudaMalloc(&d_rpart, n[0]*nr[0]*sizeof(double));
         cudaMalloc(&d_xdrange, 6*sizeof(double));
         cudaMalloc(&d_in_part, n[0]*sizeof(int));
-        cudaMalloc(&d_ipart, n[0]*ni[0]*sizeof(int));
+        cudaMalloc(&d_bc_part, 6*sizeof(int));
         cudaMemcpy(d_rpart, rpart, n[0]*nr[0]*sizeof(double), cudaMemcpyHostToDevice);
         cudaMemcpy(d_xerange, xerange, nelt[0]*6*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(d_ipart, ipart, n[0]*ni[0]*sizeof(int), cudaMemcpyHostToDevice);
         cudaMemcpy(d_in_part, in_part, n[0]*sizeof(int), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_bc_part, bc_part, 6*sizeof(int), cudaMemcpyHostToDevice);
 
 
   }
@@ -518,32 +522,32 @@ extern "C" void update_particle_location_wrapper_(double *rpart, int *ipart, dou
       d_rpart= rpart;
       d_xdrange = xdrange;
       d_in_part = in_part;
-      d_ipart = ipart;
       d_bc_part = bc_part;
-      cudaMalloc(&d_in_part, n[0]*sizeof(int));
-      cudaMemcpy(d_in_part, in_part, n[0]*sizeof(int), cudaMemcpyHostToDevice);
+//      cudaMalloc(&d_in_part, n[0]*sizeof(int));
+//      cudaMemcpy(d_in_part, in_part, n[0]*sizeof(int), cudaMemcpyHostToDevice);
 
   }
   int blockSize = 1024, gridSize;
   gridSize = (int)ceil((float)n[0]*ndim[0]/blockSize);
-  update_particle_location<<<gridSize, blockSize>>>(d_rpart,d_ipart,d_xdrange,d_inpart,d_bc_part,ndim[0],nr[0],ni[0],n[0],jx[0],jx1[0],jx2[0],jx3[0]);
+  update_particle_location<<<gridSize, blockSize>>>(d_rpart,d_xdrange,d_inpart,d_bc_part,n[0],ndim[0],nr[0],jx[0],jx1[0],jx2[0],jx3[0]);
 //----------need to confirm with Dr. Tania and Dr. Ranka if there is a way to parallelize the memory update - right now im implementing it in the host
 //  gridSize = (int)ceil((float)n[0]/blockSize);
 //  updated_data_if_outflow<<<gridSize, blockSize>>>(d_rpart,s_ipart,d_inpart,&ic);
   if(inCPU){
-      cudaMemcpy(ipart, d_ipart, n[0]*ni[0]*sizeof(int), cudaMemcpyDeviceToHost);
       cudaMemcpy(rpart, d_rpart, n[0]*nr[0]*sizeof(double), cudaMemcpyDeviceToHost);
+      cudaMemcpy(xerange, d_xerange, nelt[0]*6*sizeof(double), cudaMemcpyDeviceToHost);
       cudaMemcpy(in_part, d_in_part, n[0]*sizeof(int), cudaMemcpyDeviceToHost);
+      cudaMemcpy(bc_part, d_bc_part, 6*sizeof(int), cudaMemcpyDeviceToHost);
 
 
       //free
       cudaFree(d_rpart);
-      cudaFree(d_ipart);
+      cudaFree(d_bc_part);
       cudaFree(d_xdrange);
       cudaFree(d_in_part);
   }
   else{
-      cudaMemcpy(in_part, d_in_part, n[0]*sizeof(int), cudaMemcpyDeviceToHost);
+//      cudaMemcpy(in_part, d_in_part, n[0]*sizeof(int), cudaMemcpyDeviceToHost);
 //        if(nfpts[0]>0){
 //            cudaMemcpy(fptsmap, d_fptsmap, nfpts[0]*sizeof(int), cudaMemcpyDeviceToHost);
 //            cudaMemcpy(rfpts, d_rfpts, nfpts[0]*nrf[0]*sizeof(double), cudaMemcpyDeviceToHost);
@@ -552,7 +556,7 @@ extern "C" void update_particle_location_wrapper_(double *rpart, int *ipart, dou
 //        }
       // printf ("print var 1st %d\n", nfpts);
   }
-  cudaFree(d_in_part);
+//  cudaFree(d_in_part);
   cudaEventRecord(stopEvent, 0);
   cudaEventSynchronize(stopEvent);
   cudaEventElapsedTime(&time, startEvent, stopEvent);
@@ -561,7 +565,7 @@ extern "C" void update_particle_location_wrapper_(double *rpart, int *ipart, dou
 
 }
 
-extern "C" void interp_props_part_location_wrapper_(double *rpart,int *ipart,double *vx, double *vy, double *vz,double *t, double *vtrans, double *rep, double *xgll, double *ygll, double *zgll, double *wxgll, double *wygll, double *wzgll,int* nx1r,int* nx1, int* nr, int* ni,int* n,int* jr,int* nelt,int* ju0,int* je0,int* jtemp,int* jrho){
+extern "C" void interp_props_part_location_wrapper_(double *rpart,int *ipart,double *vx, double *vy, double *vz,double *t, double *vtrans, double *rep, double *xgll, double *ygll, double *zgll, double *wxgll, double *wygll, double *wzgll, int* n, int* nr, int* ni, int* nx1, int* nx1r, int* jr,int* ju0,int* je0,int* jtemp,int* jrho,int* nelt){
 
     float time;
     cudaEvent_t startEvent, stopEvent;
@@ -630,7 +634,7 @@ extern "C" void interp_props_part_location_wrapper_(double *rpart,int *ipart,dou
   }
   int blockSize = 1024, gridSize;
   gridSize = (int)ceil((float)n[0]/blockSize);
-  interp_props_part_location<<<gridSize, blockSize>>>(d_rpart,d_vx,d_vy,d_vz,d_t,d_vtrans,d_rep,d_xgll,d_ygll,d_zgll,d_wxgll,d_wygll,d_wzgll,nx1r[0],nx1[0],nr[0],jr[0],ju0[0],je0[0],jtemp[0],jrho[0]);
+  interp_props_part_location<<<gridSize, blockSize>>>(d_rpart,d_vx,d_vy,d_vz,d_t,d_vtrans,d_rep,d_xgll,d_ygll,d_zgll,d_wxgll,d_wygll,d_wzgll,n[0],nr[0],ni[0],nx1[0],nx1r[0],jr[0],ju0[0],je0[0],jtemp[0],jrho[0]);
   if(inCPU){
       cudaMemcpy(ipart, d_ipart, n[0]*ni[0]*sizeof(int), cudaMemcpyDeviceToHost);
       cudaMemcpy(rpart, d_rpart, n[0]*nr[0]*sizeof(double), cudaMemcpyDeviceToHost);
